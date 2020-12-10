cd73b0bbbd41e63e90ce93361c9286aa0f60d1f9
Igor Kroshkin <igor.krosh@gmail.com>
2020-12-10 14:52:25 +0300

    Механика сайдбара. Теперь сайдбар действую не на всю страницу и останавливается у футора.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,9 @@ $(document).ready(Core);
 function Core()
 {
     SetMainSlider();
+
+    $(window).on('scroll', SticlySidebar);
+    $(window).on('resize', SticlySidebarResize);
 }
 
 function SetMainSlider()
@@ -16,4 +19,43 @@ function SetMainSlider()
         //autoplay: true,
         
     });
+}
+
+function SticlySidebar()
+{
+    let windowPos = this.scrollY + window.innerHeight;
+    let sidebar = $('.sidebar');
+
+    if ((windowPos >= $('body').innerHeight() - $('footer').innerHeight()))
+    {
+        if (sidebar.hasClass('sticky'))
+        {
+            sidebar.css('position', 'absolute');
+            sidebar.css('top', $('body').innerHeight() - $('footer').innerHeight() - window.innerHeight);
+            
+            sidebar.removeClass('sticky');
+            sidebar.addClass('not-sticky');
+        }
+    }
+    else
+    {
+        if (sidebar.hasClass('not-sticky'))
+        {
+            sidebar.removeClass('not-sticky');
+            sidebar.addClass('sticky');
+            sidebar.css('position', 'fixed');
+            sidebar.css('top', '0px');
+        } 
+    }
+}
+
+function SticlySidebarResize()
+{
+    let sidebar = $('.sidebar');
+
+    if (sidebar.hasClass('not-sticky'))
+    {
+        sidebar.css('position', 'absolute');
+        sidebar.css('top', $('body').innerHeight() - $('footer').innerHeight() - window.innerHeight);
+    }
 }
